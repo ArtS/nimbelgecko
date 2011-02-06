5fad4a965c96b67b9c6d3f7d0d82e657867014dc
Artem Skvira <artem.skvira@gmail.com>
2011-02-06 20:31:06 +1100

    Moved mombers for Watcher

--- internal/watcher.js
+++ internal/watcher.js
@@ -3,13 +3,14 @@ var ng = require('ng');
 
 
 function Watcher(instance, interval) {
-    this._instance = instance,
-    this._interval = interval;
-    this._markedForDestruction = false;
+    _instance = instance,
+    _interval = interval;
+    _markedForDestruction = false;
 
     if (!(this instanceof Watcher)) {
         return new Watcher(instance, interval);
     }
+
 
     this.checkIsAlive = function() {
         if (_markedForDestruction) {
@@ -20,18 +21,16 @@ function Watcher(instance, interval) {
             setTimeout(checkIsAlive, _interval);
         }
     }
-}
 
 
+    this.startWatching = function() {
+        checkIsAlive();
+    }
 
 
-Watcher.prototype.startWatching = function() {
-    this.checkIsAlive();
-}
-
-
-Watcher.prototype.ping = function() {
-    this._markedForDestruction = false;
+    this.ping = function() {
+        _markedForDestruction = false;
+    }
 }
 
 
