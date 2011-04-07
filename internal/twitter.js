4416b47c8af731182bb6a330b956602356f63756
Artem Skvira <artem.skvira@gmail.com>
2011-04-08 01:04:56 +1000

    General cleanup/refactoring

--- internal/twitter.js
+++ internal/twitter.js
@@ -1,7 +1,8 @@
 var sys = require('sys'),
     ng = require('ng'),
     oAuth = require('node-oauth').OAuth,
-    querystring = require('querystring');
+    querystring = require('querystring'),
+    EventEmitter = require('events').EventEmitter
 
 
 function oAuthGet(url, params, callback) {
@@ -49,6 +50,8 @@ function oAuthGet(url, params, callback) {
 }
 
 
+
+
 function retrieveTweets(params, callback) {
 
     var url_params = params || {};
@@ -65,16 +68,8 @@ function retrieveTweets(params, callback) {
 }
 
 
-function startStreaming(ids_to_follow) {
-
-    var url = 'http://betastream.twitter.com/2b/site.json',
-        ids_str = ids_to_follow.join(',');
-
-    return oAuthGet(url, {with: 'followings', follow: ids_str});
-}
-
-
 function retrieveNewTweets(onNewTweetsRetrieved) {
+
     retrieveTweets(
         {},
         function(err, tweets) {
@@ -84,15 +79,114 @@ function retrieveNewTweets(onNewTweetsRetrieved) {
                 utils.retryOnError(
                     function() { exports.retrieveNewTweets(onNewTweetsRetrieved); }
                 );
-                return; 
+                return;
             }
-
             onNewTweetsRetrieved(null, tweets);
         }
     );
 }
 
-exports.oAuthGet = oAuthGet;
-exports.retrieveNewTweets = retrieveNewTweets;
-exports.retrieveTweets = retrieveTweets;
-exports.startStreaming = startStreaming;
+
+function startStreaming(ids_to_follow) {
+
+    var url = 'http://betastream.twitter.com/2b/site.json',
+        ids_str = ids_to_follow.join(',');
+
+    return oAuthGet(url, {with: 'followings', follow: ids_str});
+}
+
+
+function ReceivingStream(allUserIds) {
+
+    if (!(this instanceof ReceivingStream)) {
+        return new ReceivingStream(allUserIds)
+    }
+
+    ng.log.log('Starting receiving stream for ' + allUserIds)
+
+    var req = startStreaming(allUserIds),
+        _allUserIds = allUserIds,
+        streamRes,
+        buffer = {
+            data: '',
+            chunk: ''
+        }
+
+    if (!req) {
+        ng.log.error('Error while starting streaming.')
+        throw 'Error while starting streaming.'
+    }
+
+    req.addListener('response',
+
+        function(response) {
+
+            streamRes = response
+            streamRes.setEncoding('utf8')
+            streamRes.addListener('data', onData.bind(this))
+
+            streamRes.socket.addListener('error', onStreamError.bind(this))
+            streamRes.socket.addListener('close', onStreamClose.bind(this))
+            streamRes.socket.addListener('end', onStreamEnd.bind(this))
+
+            this.emit('stream_ready')
+
+        }.bind(this)
+    )
+
+    req.end()
+
+    //
+    // Event handlers
+    //
+
+    function onStreamClose() { ng.log.log('Stream closed.') }
+    function onStreamEnd() { ng.log.log('Stream end.') }
+    function onStreamError() { ng.log.error('Error in stream!') }
+
+    function onData(chunk) {
+        //ng.log.log("Some data from twitter...")
+        this.emit('data_arrvied')
+        processReceivedData(chunk)
+    }
+
+    function processReceivedData(chunk) {
+
+        if (chunk) { ng.log.data(chunk) }
+
+        pieces = ng.glue.glueChunksOrKeepCalm(buffer, chunk)
+        if (!pieces) {
+            return
+        }
+
+        _(pieces).each(
+            function(piece) {
+                try {
+                    js_piece = JSON.parse(piece)
+                    ng.db.saveStreamItem(js_piece)
+                } catch (err){
+                    ng.log.error('error: ' + err)
+                }
+            }
+        )
+    }
+
+    this.shutdown = function() {
+        /*
+        console.dir(this)
+        console.dir(streamRes)
+        console.dir(_allUserIds)
+        */
+        streamRes.destroy()
+        this.emit('shutdown')
+    }
+}
+
+ReceivingStream.prototype = new EventEmitter()
+
+
+exports.oAuthGet = oAuthGet
+exports.retrieveNewTweets = retrieveNewTweets
+exports.retrieveTweets = retrieveTweets
+exports.startStreaming = startStreaming
+exports.ReceivingStream = ReceivingStream
--- receiver.js
+++ receiver.js
@@ -12,137 +12,16 @@ require('underscore')
 
 
 var ng = require('ng'),
-    EventEmitter = require('events').EventEmitter,
     runChain = require('node-chain').runChain,
-    init_chain,
     STREAM_CHECK_INTERVAL = 60000
 
-
-function ReceivingStream(allUserIds) {
-
-    if (!(this instanceof ReceivingStream)) {
-        return new ReceivingStream(allUserIds)
-    }
-
-    ng.log.log('Starting receiving stream for ' + allUserIds)
-
-    var req = ng.twitter.startStreaming(allUserIds),
-        _allUserIds = allUserIds,
-        streamRes,
-        buffer = {
-            data: '',
-            chunk: ''
-        }
-
-    if (!req) {
-        ng.log.error('Error while starting streaming.')
-        throw 'Error while starting streaming.'
-    }
-
-    req.addListener('response',
-
-        function(response) {
-
-            streamRes = response
-            streamRes.setEncoding('utf8')
-            streamRes.addListener('data', onData.bind(this))
-
-            streamRes.socket.addListener('error', onStreamError.bind(this))
-            streamRes.socket.addListener('close', onStreamClose.bind(this))
-            streamRes.socket.addListener('end', onStreamEnd.bind(this))
-
-            this.emit('stream_ready')
-
-        }.bind(this)
-    )
-
-    req.end()
-
-    //
-    // Event handlers
-    //
-
-    function onStreamClose() { ng.log.log('Stream closed.') }
-    function onStreamEnd() { ng.log.log('Stream end.') }
-    function onStreamError() { ng.log.error('Error in stream!') }
-
-    function onData(chunk) {
-        //ng.log.log("Some data from twitter...")
-        this.emit('data_arrvied')
-        processReceivedData(chunk)
-    }
-
-    function processReceivedData(chunk) {
-
-        if (chunk) { ng.log.data(chunk) }
-
-        pieces = ng.glue.glueChunksOrKeepCalm(buffer, chunk)
-        if (!pieces) {
-            return
-        }
-
-        _(pieces).each(
-            function(piece) {
-                try {
-                    js_piece = JSON.parse(piece)
-                    ng.db.saveStreamItem(js_piece)
-                } catch (err){
-                    ng.log.error('error: ' + err)
-                }
-            }
-        )
-    }
-
-    this.shutdown = function() {
-        /*
-        console.dir(this)
-        console.dir(streamRes)
-        console.dir(_allUserIds)
-        */
-        streamRes.destroy()
-        this.emit('shutdown')
-    }
-}
-
-
-ReceivingStream.prototype = new EventEmitter()
-
-
-function startReceivingStream(allUserIds) {
-    var stream = new ReceivingStream(allUserIds),
-        watcher = new ng.watcher.Watcher(STREAM_CHECK_INTERVAL)
-
-    stream.on('stream_ready', function() { watcher.startWatching(stream) } )
-    stream.on('data_arrvied', function() { watcher.keepAlive() } )
-    stream.on('shutdown', runStreamingChain)
-}
-
-
-//
-// Starts receiving stream from twitter
-//
-function runStreamingChain() {
-    var stream_chain = [
-        {
-            target: ng.db.getAllUserIds,
-            errorMessage: 'Error obtaining all user IDs',
-            passResultToNextStep: true
-        }
-      , {
-            target: startReceivingStream,
-            errorMessage: 'Error when starting receiving stream'
-        }
-    ]
-
-    runChain(stream_chain)
-}
 
 
 //
 // Inits config engine, database and then kicks off
 // receiving stream initialisation chain
 //
-init_chain = [
+runChain([
     {
         target: ng.conf.initConfig,
         errorMessage: 'Unable to init the config'
@@ -153,9 +32,36 @@ init_chain = [
         errorMessage: 'Database initialisation failed.'
     },
     {
-        target: runStreamingChain,
+        target: startReceivingAllTweets,
         errorMessage: 'Error starting streaming'
     }
-]
+])
 
-runChain(init_chain)
+
+//
+// Starts receiving all tweets from twitter in streaming mode
+//
+function startReceivingAllTweets() {
+
+    function startReceivingStream(allUserIds) {
+
+        var stream = new ng.twitter.ReceivingStream(allUserIds)
+          , watcher = new ng.watcher.Watcher(STREAM_CHECK_INTERVAL)
+
+        stream.on('stream_ready', function() { watcher.startWatching(stream) } )
+        stream.on('data_arrvied', function() { watcher.keepAlive() } )
+        stream.on('shutdown', startReceivingAllTweets)
+    }
+
+    runChain([
+        {
+            target: ng.db.getAllUserIds,
+            errorMessage: 'Error obtaining all user IDs',
+            passResultToNextStep: true
+        }
+      , {
+            target: startReceivingStream,
+            errorMessage: 'Error when starting receiving stream'
+        }
+    ])
+}
