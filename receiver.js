dc0c5d5a999eb638a56e02b53359189197a60395
Artem Skvira <artem.skvira@gmail.com>
2011-05-19 19:41:42 +1000

    Added restart for unhandled exception in receiver

--- receiver.js
+++ receiver.js
@@ -9,27 +9,6 @@ var ng = require('ng'),
     STREAM_CHECK_INTERVAL = 60000
 
 //
-// Inits config engine, database and then kicks off
-// receiving stream initialisation chain
-//
-runChain([
-    {
-        target: ng.conf.initConfig,
-        errorMessage: 'Unable to init the config'
-    },
-    {
-        target: ng.db.initDatabase,
-        args: [['tweets', 'users', 'other']],
-        errorMessage: 'Database initialisation failed.'
-    },
-    {
-        target: startReceivingAllTweets,
-        errorMessage: 'Error starting streaming'
-    }
-])
-
-
-//
 // Starts receiving all tweets from twitter in streaming mode
 //
 function startReceivingAllTweets() {
@@ -56,3 +35,35 @@ function startReceivingAllTweets() {
         }
     ])
 }
+
+//
+// Inits config engine, database and then kicks off
+// receiving stream initialisation chain
+//
+function startReceiver() {
+    runChain([
+        {
+            target: ng.conf.initConfig,
+            errorMessage: 'Unable to init the config'
+        },
+        {
+            target: ng.db.initDatabase,
+            args: [['tweets', 'users', 'other']],
+            errorMessage: 'Database initialisation failed.'
+        },
+        {
+            target: startReceivingAllTweets,
+            errorMessage: 'Error starting streaming'
+        }
+    ])
+}
+
+
+process.on('uncaughtException',
+    function(err) {
+        ng.log.error(err, 'Unhandled exception')
+        startReceiver()
+    }
+)
+
+startReceiver()
