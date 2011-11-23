d5699376989952d6481486b0149bff6a881037df
Artem Skvira <artem.skvira@gmail.com>
2011-11-23 22:27:41 +1100

    Added avatars

--- static/media/js/ng/home.js
+++ static/media/js/ng/home.js
@@ -15,6 +15,9 @@ $(document).ready(function() {
       , singleTweetDirective = {
             'li': {
                 'tweet<-tweets': {
+                    'div.avatar@style': function(ctx) {
+                        return 'background-image: url(' + ctx.item.user.profile_image_url + ')'
+                    },
                     'span.text': 'tweet.escaped_text',
                     'span.tweet-link a@href':
                     'http://twitter.com/#{tweet.user.screen_name}/statuses/#{tweet.id_str}',
